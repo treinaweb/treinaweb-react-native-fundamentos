acde33ea0e3afa4053ce6ba8a6135d8d15397341
hanashiro <hanashiro@Felipes-MacBook-Pro.local>
2019-02-07 15:18:09 -0200

    aula-05-02

--- TextCounter.js
+++ TextCounter.js
@@ -2,15 +2,21 @@ import React, {Component} from 'react';
 import { View, Text, TextInput } from 'react-native';
 
 class TextCounter extends Component{
+    state = {
+        text: 'abc'
+    }
+
     render(){
+        const { state } = this;
         return (
             <View>
                 <TextInput
                     multiline={true}
                     numberOfLines={5}
+                    value={state.text}
                     style={{borderColor: 'gray', borderWidth: 1}}
                 />
-                <Text>Total: 20/100</Text>
+                <Text>Total: { state.text.length }/100</Text>
             </View>
         );
     }
